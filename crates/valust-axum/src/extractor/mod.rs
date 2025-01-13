48bf1a4f041661451304ba0515e67e78e255ed0c
Embers-of-the-Fire <stellarishs@163.com>
2025-01-13 17:21:27 +0800

    add: add `sonic` json support

--- crates/valust-axum/src/extractor/mod.rs
+++ crates/valust-axum/src/extractor/mod.rs
@@ -1,13 +1,48 @@
 //! [`axum`]-compatible extractor.
 
 #[cfg(feature = "json")]
+#[cfg_attr(docsrs, doc(cfg(feature = "json")))]
 pub mod json;
+use axum::http::{header, HeaderMap};
 #[cfg(feature = "json")]
+#[cfg_attr(docsrs, doc(cfg(feature = "json")))]
 pub use json::ValidJson;
 
 #[cfg(feature = "form")]
+#[cfg_attr(docsrs, doc(cfg(feature = "form")))]
 pub mod form;
 #[cfg(feature = "form")]
+#[cfg_attr(docsrs, doc(cfg(feature = "form")))]
 pub use form::ValidForm;
 
+#[cfg(feature = "sonic")]
+#[cfg_attr(docsrs, doc(cfg(feature = "sonic")))]
+pub mod sonic;
+use mime::Mime;
+#[cfg(feature = "sonic")]
+#[cfg_attr(docsrs, doc(cfg(feature = "sonic")))]
+pub use sonic::ValidSonic;
+
 pub mod rejection;
+
+fn check_content_type(headers: &HeaderMap, expected_content_type: Mime) -> bool {
+    let content_type = if let Some(content_type) = headers.get(header::CONTENT_TYPE) {
+        content_type
+    } else {
+        return false;
+    };
+
+    let content_type = if let Ok(content_type) = content_type.to_str() {
+        content_type
+    } else {
+        return false;
+    };
+
+    let mime = if let Ok(mime) = content_type.parse::<Mime>() {
+        mime
+    } else {
+        return false;
+    };
+
+    mime == expected_content_type
+}
---
+++ crates/valust-axum/src/extractor/sonic.rs
@@ -0,0 +1,92 @@
+//! Parse raw data from JSON with [`sonic`][sonic_rs] and validate it.
+
+use std::ops::{Deref, DerefMut};
+
+use axum::body::Bytes;
+use axum::extract::{FromRequest, Request};
+use serde::de::DeserializeOwned;
+use valust::Validate;
+
+use super::check_content_type;
+use super::rejection::ValidateRejection;
+
+/// Wrapper around a validated JSON value.
+///
+/// ```rust,no_run
+/// use axum::{
+///     routing::post,
+///     Router,
+/// };
+/// use valust_axum::extractor::ValidSonic;
+/// use valust_derive::Valust;
+/// use serde::Deserialize;
+///
+/// #[derive(Valust)]
+/// // we need to derive `Deserialize` to use `ValidSonic`
+/// #[forward_derive(Deserialize)]
+/// struct CreateUser {
+///     #[valid(email.contains("@"))]
+///     email: String,
+///     #[valid(password.len() > 8)]
+///     password: String,
+/// }
+///
+/// async fn create_user(ValidSonic(payload): ValidSonic<CreateUser>) {
+///     // payload is a `CreateUser`
+/// }
+///
+/// let app = Router::new().route("/users", post(create_user));
+/// # let _: Router = app;
+/// ```
+#[derive(Debug, Clone, Copy, Default)]
+#[must_use]
+pub struct ValidSonic<T>(pub T);
+
+impl<T> Deref for ValidSonic<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<T> DerefMut for ValidSonic<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+impl<T> AsRef<T> for ValidSonic<T> {
+    fn as_ref(&self) -> &T {
+        &self.0
+    }
+}
+
+impl<T> AsMut<T> for ValidSonic<T> {
+    fn as_mut(&mut self) -> &mut T {
+        &mut self.0
+    }
+}
+
+impl<T, S> FromRequest<S> for ValidSonic<T>
+where
+    S: Send + Sync,
+    T: Validate,
+    T::Raw: DeserializeOwned,
+{
+    type Rejection = ValidateRejection<sonic_rs::Error>;
+
+    async fn from_request(req: Request, state: &S) -> Result<Self, Self::Rejection> {
+        if !check_content_type(req.headers(), mime::APPLICATION_JSON) {
+            return Err(ValidateRejection::UnsupportedMediaType(mime::APPLICATION_JSON));
+        }
+
+        let raw = Bytes::from_request(req, state).await?;
+        let json = sonic_rs::from_slice(&raw)
+            .map_err(ValidateRejection::InvalidContentFormat)?;
+
+        let data = T::validate(json).map_err(ValidateRejection::InvalidValue)?;
+
+        Ok(ValidSonic(data))
+    }
+}
