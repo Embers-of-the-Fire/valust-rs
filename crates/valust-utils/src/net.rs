03be701c98fbf67544d93f92757c1631fc3bd104
Embers-of-the-Fire <stellarishs@163.com>
2025-01-24 10:14:47 +0800

    add: add socket-address valid/trans

--- crates/valust-utils/src/net.rs
+++ crates/valust-utils/src/net.rs
@@ -1,6 +1,8 @@
 //! Network related validators.
 
-use std::net::{AddrParseError, IpAddr, Ipv4Addr, Ipv6Addr};
+use std::net::{
+    AddrParseError, IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr, SocketAddrV4, SocketAddrV6,
+};
 
 /// Check if a string-like item is a valid IP address.
 ///
@@ -98,5 +100,104 @@ pub fn is_ipv6(s: impl AsRef<str>) -> bool {
 /// }
 /// ```
 pub fn ipv6(s: impl AsRef<str>) -> Result<Ipv6Addr, AddrParseError> {
+    s.as_ref().parse()
+}
+
+/// Check if a string-like item is a valid Socket address.
+///
+/// ```rust
+/// # use valust_utils::net::is_sock;
+/// # use valust::{Validate, Raw, error::display::ErrorDisplay};
+/// # use valust_derive::Valust;
+/// #
+/// #[derive(Valust)]
+/// struct Stringify {
+///     #[valid(func(is_sock))]
+///     ip: String,
+/// }
+/// ```
+pub fn is_sock(s: impl AsRef<str>) -> bool {
+    s.as_ref().parse::<SocketAddr>().is_ok()
+}
+
+/// Convert a string-like item into a Socket address.
+///
+/// ```rust
+/// # use valust::{Validate, Raw, error::display::ErrorDisplay};
+/// # use valust_derive::Valust;
+/// use valust_utils::net::sock;
+/// #[derive(Valust)]
+/// struct Stringify {
+///     #[trans(func(String => try(sock)))]
+///     socket: std::net::SocketAddr,
+/// }
+/// ```
+pub fn sock(s: impl AsRef<str>) -> Result<SocketAddr, AddrParseError> {
+    s.as_ref().parse()
+}
+
+/// Check if a string-like item is a valid SocketV4 address.
+///
+/// ```rust
+/// # use valust_utils::net::is_sockv4;
+/// # use valust::{Validate, Raw, error::display::ErrorDisplay};
+/// # use valust_derive::Valust;
+/// #
+/// #[derive(Valust)]
+/// struct Stringify {
+///     #[valid(func(is_sockv4))]
+///     socket_v4: String,
+/// }
+/// ```
+pub fn is_sockv4(s: impl AsRef<str>) -> bool {
+    s.as_ref().parse::<SocketAddrV4>().is_ok()
+}
+
+/// Convert a string-like item into a SocketV4 address.
+///
+/// ```rust
+/// # use valust::{Validate, Raw, error::display::ErrorDisplay};
+/// # use valust_derive::Valust;
+/// use valust_utils::net::sockv4;
+/// #[derive(Valust)]
+/// struct Stringify {
+///     #[trans(func(String => try(sockv4)))]
+///     socket_v4: std::net::SocketAddrV4,
+/// }
+/// ```
+pub fn sockv4(s: impl AsRef<str>) -> Result<SocketAddrV4, AddrParseError> {
+    s.as_ref().parse()
+}
+
+/// Check if a string-like item is a valid SocketV6 address.
+///
+/// ```rust
+/// # use valust_utils::net::is_sockv6;
+/// # use valust::{Validate, Raw, error::display::ErrorDisplay};
+/// # use valust_derive::Valust;
+/// #
+/// #[derive(Valust)]
+/// struct Stringify {
+///     #[valid(func(is_sockv6))]
+///     socket_v6: String,
+/// }
+/// ```
+pub fn is_sockv6(s: impl AsRef<str>) -> bool {
+    s.as_ref().parse::<SocketAddrV6>().is_ok()
+}
+
+/// Convert a string-like item into an IPv6 address.
+///
+/// ```rust
+/// # use valust::{Validate, Raw, error::display::ErrorDisplay};
+/// # use valust_derive::Valust;
+/// use valust_utils::net::sockv6;
+/// #[derive(Valust)]
+/// struct Stringify {
+///     #[trans(func(String => try(sockv6)))]
+///     socket_v6: std::net::SocketAddrV6,
+/// }
+/// ```
+pub fn sockv6(s: impl AsRef<str>) -> Result<SocketAddrV6, AddrParseError> {
     s.as_ref().parse()
 }
